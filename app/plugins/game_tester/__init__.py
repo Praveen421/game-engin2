685bee1ac392b61e8f1e18f19c92c9e8c2484860
sidd5sci <sidd5sci@gmail.com>
2018-10-04 18:58:53 +0530

    add

--- app/plugins/game_tester/__init__.py
+++ app/plugins/game_tester/__init__.py
@@ -1,6 +1,15 @@
 
+### meta data
+
+author      = "siddhartha"
+name        = 'level designer'
+version     = '1.0.1'
+main        = 'main.py'
+
+load_sequence =[]
+
 class Plugin:
     def __init__(self,*args,**kwargs):
-        print('Game Tester :',args,kwargs)
+        print('Level designer :',args,kwargs)
     def run(self):
         print("run")
