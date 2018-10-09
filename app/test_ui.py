0fc9f01ffcbed7c0fba6e31f81783ede99b3ef42
sidd5sci <sidd5sci@gmail.com>
2018-10-10 01:52:05 +0530

    new ui testing

--- app/test_ui.py
+++ app/test_ui.py
@@ -7,7 +7,7 @@
 ## PLEASE DO *NOT* EDIT THIS FILE!
 ###########################################################################
 
-import wx
+import wx,sys
 import wx.xrc
 import wx.aui
 
@@ -15,47 +15,40 @@ import wx.aui
 ## Class MyFrame1
 ###########################################################################
 
-class MyFrame1 ( wx.Frame ):
+class MyFrame(wx.Frame):
 
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 736,423 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+	def __init__(self,parent, title):
+			super(MyFrame, self).__init__(parent, title=title, 
+            								size=(250, 550))
+			self.fuck()
+	def fuck(self):
 
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+			import sys,os
+			##Note we call the GetHandle() method of a control in the window/frame, not the wxFrame itself
+			self.hwnd = self.GetChildren()
+			if sys.platform == "win32":
+				os.environ['SDL_VIDEODRIVER'] = 'windib'
+			os.environ['SDL_WINDOWID'] = str(self.hwnd) #must be before init
 
-		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+			## NOTE WE DON'T IMPORT PYGAME UNTIL NOW.  Don't put "import pygame" at the top of the file.
+			import pygame
+			pygame.display.init()
 
-		self.m_panel1 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
+			self.display = pygame.display.set_mode((400,400)) #size no matter
 
-		self.m_auinotebook1 = wx.aui.AuiNotebook( self.m_panel1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.aui.AUI_NB_DEFAULT_STYLE )
+			pygame.display.set_caption("PyTrack | v1.0.1")
+			# initilise the clock
+			self.clock = pygame.time.Clock()
+		
+			self.screen.fill(pygame.Color(255,255,255))
+			pygame.display.init()
+			pygame.display.update()
+    
 
-		bSizer2.Add( self.m_auinotebook1, 1, wx.EXPAND |wx.ALL, 5 )
-
-
-		self.m_panel1.SetSizer( bSizer2 )
-		self.m_panel1.Layout()
-		bSizer2.Fit( self.m_panel1 )
-		bSizer1.Add( self.m_panel1, 1, wx.EXPAND |wx.ALL, 5 )
-
-		self.m_panel2 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		bSizer1.Add( self.m_panel2, 1, wx.EXPAND |wx.ALL, 5 )
-
-
-		self.SetSizer( bSizer1 )
-		self.Layout()
-
-		self.Centre( wx.BOTH )
-
-	def __del__( self ):
-		pass
-
-
-
-	
 
 if __name__ == '__main__':
    
         app = wx.App(False)
-        frame = MyFrame1(None)
+        frame = MyFrame(None,"hj")
         frame.Show()
         app.MainLoop()
